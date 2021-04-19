0930d646a9ed0899928f87878dac148289fa563b
root <root@vps-da0a2860.vps.ovh.net>
2021-04-19 10:12:26 +0000

    ajoute mkdir,apache

--- script.sh
+++ script.sh
@@ -10,15 +10,29 @@ echo "Config snapd + certbot"
 snap install core
 snap refresh core
 snap install --classic certbot
-
+service apache2 start
 CERTBOT=$(ls /usr/bin | grep certbot)
 if [ -z "$CERTBOT" ]
 then
-	        echo" on cree le lien /usr/bin/certbot
+	        echo " on cree le lien /usr/bin/certbot"
 		ln -s /snap/bin/certbot /usr/bin/certbot
 fi
 
+mkdir -p /var/www/html
 
+CHECK_GIT_CONFIG=$(cd /var/www/html && git config --get remote.origin.url)
+if [ -z "$CHECK_GIT_CONFIG" ]
+then
+	  echo "Mise en place config git"
+	    cd /var/www/html && rm -rf .git/
+	      git init
+	      git remote add origin https://github.com/Martinko33/KodeMe.git
+fi
+APACHE_CHECK=$(ls /etc/apache2/sites-available/ | grep 000-default.conf)
+	if [ -z "$APACHE_CHECK" ]
+	then
+		touch /etc/apache2/sites-available/000-default.conf
+	fi
 
 #je dois comparer deux numero ,on recoupere identifiant par md5sum de notre 000-default.conf
 #on le mettre dans variable du coup MD5_DEST et MD5_SRC
@@ -26,7 +40,7 @@ MD5_DEST=$(md5sum /etc/apache2/sites-available/000-default.conf | awk '{print $1
 MD5_SRC=$(md5sum 000-default.conf |awk '{print $1}')
 if [ "$MD5_DEST" != "$MD5_SRC" ]
 then
-      	echo"on ecrase la conf apache"
+      	echo "on ecrase la conf apache"
 	 cp 000-default.conf /etc/apache2/sites-available/000-default.conf
         service apache2 restart
 fi
